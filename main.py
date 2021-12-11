5f17272b797991bf5b57d2ccd35059fa5e439988
wehzie <39304339+Wehzie@users.noreply.github.com>
2021-12-11 12:28:20 +0200

    binarized all data

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 import aif360.sklearn.metrics as metrics
+from sklearn.utils.sparsefuncs import inplace_column_scale
 
 VERBOSE = True # verbose training
 SEED = 123
@@ -16,26 +17,35 @@ TRAIN_DATA_PATH = Path("./data_train.csv")
 TRAIN_TEST_SPLIT = 0.9 # 90% train
 COLUMNS = ["age", "country", 
     "sex", "smoking", "background_diseases_binary",
-    "background_diseases_diabetes",
-    "background_diseases_hypertension",
-    "background_diseases_kidney_failure",
-    "background_diseases_asthma",
-    "background_diseases_cardiovascular",
-    "background_diseases_obesity",
-    "background_diseases_immunosuppression",
-    "background_diseases_chronic_obstructive_pulmonary",
     "severity_illness"]
+MAIN_COUNTRY = "colombia" # country with most data. colombia = 0, other = 1
 TARGET = "severity_illness" # target variable
 TARGET_NAMES_MULTI = ["asymptomatic", "critical", "cured", "deceased", "good"] # oder matters
 TARGET_POS = ["asymptomatic", "cured"]
 TARGET_NEG = ["critical", "deceased", "good"]
 TARGET_NAMES = ["0", "1"] # 0: cheap, 1: expensive
+PRIV_GENDER = "male" # 0
+UNPRIV_GENDER = "female" # 1
+PRIV_AGE = range(70) # 0
+UNPRIV_AGE = range(70, 200) # 1
 
 
 def import_data(train_path = TRAIN_DATA_PATH, nrows = NUMBER_OF_ROWS) -> pd.DataFrame:
     """load CSVs into dataframes"""
     df = pd.read_csv(train_path, nrows=nrows)
     return df
+
+def get_other_countries(data, MAIN_COUNTRY):
+    all_countries = set(data["country"])
+    all_countries.remove(MAIN_COUNTRY)
+    return all_countries
+
+def remove_transgender(data) -> None:
+    data.drop(data[data.sex == "transgender"].index, inplace=True)
+
+def binarize_gender(data, PRIV_GENDER, UNPRIV_GENDER):
+    data.replace(PRIV_GENDER, 0, inplace=True) # men: 0
+    data.replace(UNPRIV_GENDER, 1, inplace=True) # women: 1
 
 def filter_data(data, keep_cols = COLUMNS) -> pd.DataFrame:
     """remove unwanted columns"""
@@ -53,6 +63,14 @@ def binarize_target(data, pos = TARGET_POS, neg = TARGET_NEG) -> np.ndarray:
     data = data.replace(pos, 0) # cheap: asymptomatic, cured
     data = data.replace(neg, 1) # expensive: deceased, good, critical
     return data
+
+def binarize_country(data, MAIN_COUNTRY, other_countries):
+    data["country"].replace(other_countries, 1, inplace=True)
+    data["country"].replace(MAIN_COUNTRY, 0, inplace=True)
+
+def binarize_age(data, PRIV_AGE, UNPRIV_AGE):
+    data["age"].replace(PRIV_AGE, 0, inplace=True)
+    data["age"].replace(UNPRIV_AGE, 1, inplace=True)
 
 def clean_data(data) -> pd.DataFrame:
     """apply one hot encoding and replace NaN entries"""
@@ -113,12 +131,19 @@ def metrics(X, y, t = TARGET, pos_label = 1):
     return (gender(diff), smoking(diff), background(diff), age(diff),
             gender(div), smoking(div), background(div), age(div),)
 
+
+
 def main(tn = TARGET_NAMES):
     data = import_data()
+    other_countries = get_other_countries(data, MAIN_COUNTRY)
+    remove_transgender(data)
+    binarize_gender(data, PRIV_GENDER, UNPRIV_GENDER)
     data = filter_data(data)
     # first split X and y, then preprocess, then split train and test!
     X, y = separate_X_y(data)
     y = binarize_target(y)
+    binarize_country(X, MAIN_COUNTRY, other_countries)
+    binarize_age(X, PRIV_AGE, UNPRIV_AGE)
     X = clean_data(X)
     print(X.columns)
     X_train, X_test, y_train, y_test = split_train_test(X, y) 
@@ -126,9 +151,7 @@ def main(tn = TARGET_NAMES):
     labels = test_model(X_test, model)
 
     print(X_train)
-    # split countries by HDI
-    # remove individual diseases
-    # remove transgender
+
     # age old >= 70, age young < 70
     
     print(classification_report(y_true=y_test, y_pred=labels, zero_division=0, target_names=tn))
